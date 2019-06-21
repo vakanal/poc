7b9db74cdde2651781924a9676ec01ce7bdbdd81
AMC <antoniomcaballero@gmail.com>
2019-06-21 12:47:01 +0200

    UPDATING - Background image basic test

--- src/components/Container.tsx
+++ src/components/Container.tsx
@@ -1,10 +1,16 @@
 import * as React from "react";
 
+const imagenURL = "https://images.pexels.com/photos/926987/pexels-photo-926987.jpeg?cs=srgb&dl=america-analysis-cellphone-926987.jpg&fm=jpg";
+
 const style = (center: boolean, mainHeight: boolean): React.CSSProperties => ({
   alignItems: center ? "center" : undefined,
   backgroundColor: "#eee",
+  // backgroundImage: "url(" + "https://images.pexels.com/photos/926987/pexels-photo-926987.jpeg?cs=srgb&dl=america-analysis-cellphone-926987.jpg&fm=jpg" + ")",
+  backgroundImage: `url("${imagenURL}")`,
+  backgroundRepeat: "no-repeat",
+  backgroundSize: "cover",
   display: "flex",
-  flexDirection: "column",
+  flexDirection: "column",  
   height: mainHeight ? "calc(100vh - 60px)" : "calc(100vh - 20px)",
   justifyContent: center ? "center" : undefined,
   padding: "10px 15px",
@@ -13,12 +19,12 @@ const style = (center: boolean, mainHeight: boolean): React.CSSProperties => ({
 
 interface IContainerProps {
   center?: boolean;
-  mainHeight?: boolean;
+  mainHeight?: boolean;  
 }
 
 export default class Container extends React.Component<IContainerProps> {
   public render() {
-    const { children, center = false, mainHeight = false } = this.props;
+    const {children, center = false, mainHeight = false } = this.props;
     return <div style={style(center, mainHeight)}>{children}</div>;
   }
 }
