4e2d970b35037b4d46484615a2c83f522350b0e5
javoliful <javiof@gmail.com>
2019-06-24 12:55:36 +0200

    - react dev tools

--- src/index.tsx
+++ src/index.tsx
@@ -4,7 +4,7 @@ import * as ReactDOM from "react-dom";
 import { createBrowserHistory } from "history";
 import { Provider } from "react-redux";
 import { Router } from "react-router";
-import { applyMiddleware, combineReducers, createStore } from "redux";
+import { applyMiddleware, combineReducers, compose, createStore   } from "redux";
 import { reducer as formReducer } from "redux-form";
 
 import thunk from "redux-thunk";
@@ -15,12 +15,21 @@ import App from "./App";
 import "./index.css";
 import registerServiceWorker from "./registerServiceWorker";
 
+declare global {
+  interface Window {
+    __REDUX_DEVTOOLS_EXTENSION_COMPOSE__?: typeof compose;
+  }
+}
+ 
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+
 const store = createStore(
   combineReducers({
     ...reducers,
     form: formReducer
   }),
-  applyMiddleware(thunk.withExtraArgument(services))
+  composeEnhancers(applyMiddleware(thunk.withExtraArgument(services)))
 );
 
 const history = createBrowserHistory();
